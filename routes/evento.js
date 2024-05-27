bd5bf5ed6eb73e5bb1d71b903576dd2acbcd89e8
Ana Carolina Jungbluth <120262119+AnaJungbluth@users.noreply.github.com>
2024-05-27 15:26:08 -0300

    Add files via upload

--- routes/evento.js
+++ routes/evento.js
@@ -3,34 +3,29 @@ const Evento = require('../model/Evento');
 const isAuthorized = require('../middleware/isAuthorized');
 const router = express.Router();
 
-//ROTAS
+// ROTAS
 router.get('/', isAuthorized, async function(req, res) {
-  return res.json(await Evento.find())
+  return res.json(await Evento.find());
 });
 
-//Obter um evento por id
+// Obter um evento por id
 router.get("/:id", isAuthorized, async (req, res) => {
-  const {id} = req.params
+  const { id } = req.params;
+  const result = await Evento.findById(id);
 
-  const result = await Evento.findById(id)
+  return result ? res.json(result) : res.status(401).send();
+});
 
-  return result
-    ? res.json(result)
-    : res.status(401).send()
-})
-
-router.post("/", async (req, res) => {
+router.post("/", isAuthorized, async (req, res) => {
     const body = req.body;
-    const evento = new Evento(body)
-    const hasErrors = evento.validateSync()
+    const evento = new Evento(body);
+    const hasErrors = evento.validateSync();
 
-    return hasErrors
-      ? res.status(400).json(hasErrors)
-      : res.status(201).json(await evento.save())
-})
+    return hasErrors ? res.status(400).json(hasErrors) : res.status(201).json(await evento.save());
+});
 
 router.put("/:id", isAuthorized, async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
   const eventData = req.body;
 
   try {
@@ -44,24 +39,23 @@ router.put("/:id", isAuthorized, async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
-})
+});
 
 router.delete("/:id", isAuthorized, async (req, res) => {
     const { id } = req.params;
 
     try {
-      // Encontre o evento pelo ID e remova-o
       const deletedEvent = await Evento.findByIdAndDelete(id);
-  
+
       if (!deletedEvent) {
         return res.status(404).json({ error: "Event not found" });
       }
-  
+
       res.json({ message: "Event deleted successfully" });
     } catch (error) {
       res.status(500).json({ error: "Internal Server Error" });
     }
-})
+});
 
-//EXPORTA DO MODULO
+// EXPORTA DO MÓDULO
 module.exports = router;
